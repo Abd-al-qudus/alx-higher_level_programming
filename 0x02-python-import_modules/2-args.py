9a703dd5997389037b1f57803afe3cc2712302a7
Abd-al-qudus <jimohqudus86@gmail.com>
2022-12-01 12:37:01 +0100

    python import

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv) - 1
+    if length == 0:
+        print(f"{length} arguments.")
+    elif length == 1:
+        print(f"{length} argument:\n {length}: {sys.argv[length]}")
+    else:
+        print(f"{length} arguments")
+        for i in range(1, length + 1):
+            print(f"{i}: {sys.argv[i]}")
