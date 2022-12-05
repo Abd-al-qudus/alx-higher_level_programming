a178137354aa3acea44892621b892d643dbbcf18
Abd-al-qudus <jimohqudus86@gmail.com>
2022-12-05 11:41:12 +0100

    update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,3 +5,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple_b + (0, 0)
         answer = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
         return answer
+    else:
+        return 0, 0
