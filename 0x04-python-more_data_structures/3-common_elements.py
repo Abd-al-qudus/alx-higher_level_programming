89000d9b2e3d77da633e02301e1e17d7cb51c4fe
Abd-al-qudus <jimohqudus86@gmail.com>
2022-12-07 12:19:35 +0100

    update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     if set_1 or set_2:
-        common = {el for el in set_1 if el in set_2}
-        return common
+        return set(set_1 & set_2)
