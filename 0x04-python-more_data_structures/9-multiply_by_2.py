1952572856919a2f4acd1a23947fc22cb977ff82
Abd-al-qudus <jimohqudus86@gmail.com>
2022-12-07 12:47:48 +0100

    update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary:
-        new_dict = a_dictionary.copy()
-        for key in list(new_dict.keys()):
-            new_dict[key] *= 2
-        return new_dict
+    new_dict = a_dictionary.copy()
+    for key in list(new_dict.keys()):
+        new_dict[key] *= 2
+    return new_dict
