4677466807a3922179d1fb7ea7b3e2aff9122e67
Abd-al-qudus <jimohqudus86@gmail.com>
2023-03-18 14:49:51 +0100

    fixed sql task 4 and 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
+cursor = db.cursor()
+query = "SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id \
+    WHERE states.name LIKE %s ORDER BY cities.id ASC"
+search = sys.argv[4]
+cursor.execute(query, [search])
+for row in cursor.fetchall():
+    print(f"{row}")
+db.close()
