3837af3a6e0513a5ee672d80f11df625d10fe85f
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-28 16:38:57 +0100

    web scraping fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const filename = process.argv[2];
 const filecontent = process.argv[3];
-fs.writeFile(filename, filecontent, 'utf-8', function (error){
-  if (error){
+fs.writeFile(filename, filecontent, 'utf-8', function (error) {
+  if (error) {
     console.log(error);
   }
-})
+});
