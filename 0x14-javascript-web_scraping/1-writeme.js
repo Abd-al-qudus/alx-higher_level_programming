7259883cf65f8d48124f50503bf5b848b0a66f57
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-28 15:52:04 +0100

    web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filename = process.argv[2];
-console.log(process.argv[2]);
-const text = process.argv[3];
-fs.writeFile(filename, text, 'utf-8', function (error){
+const filecontent = process.argv[3];
+fs.writeFile(filename, filecontent, 'utf-8', function (error){
   if (error){
     console.log(error);
   }
