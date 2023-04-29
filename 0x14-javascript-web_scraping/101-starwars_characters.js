0d996d1375331e5fe5503991e7b8ddbd0e6face0
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-29 08:58:18 +0100

    fixed async request for task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,29 +3,35 @@ const request = require('request');
 const charId = process.argv[2];
 const moviesAPI = 'https://swapi-api.alx-tools.com/api/films/';
 
-function getName (url, callback) {
-  request(url, (error, resp, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const charName = JSON.parse(body).name;
-      callback(null, charName);
-    }
+async function getName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, resp, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const charName = JSON.parse(body).name;
+        resolve(charName);
+        console.log(charName);
+      }
+    });
   });
 }
 
-request(moviesAPI + charId, function (error, resp, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const charactersList = JSON.parse(body).characters;
-    for (let i = 0; i < charactersList.length; i++) {
-      getName(charactersList[i], (error, data) => {
-        if (error) {
-          console.log(error);
+async function main () {
+  try {
+    request(moviesAPI + charId, async function (error, resp, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        const charactersList = JSON.parse(body).characters;
+        for (let i = 0; i < charactersList.length; i++) {
+          await getName(charactersList[i]);
         }
-        console.log(data);
-      });
-    }
+      }
+    });
+  } catch (error) {
+    console.log(error);
   }
-});
+}
+
+main();
