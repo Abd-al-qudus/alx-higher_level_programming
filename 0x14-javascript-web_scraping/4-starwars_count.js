da8dff9e38b0a8f66a5fa8d0ec62b87a877624a4
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-29 04:40:41 +0100

    updated star war character count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!usr/bin/node
+const request = require('request');
+const API = process.argv[2];
+request.get(API, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const moviesList = JSON.parse(body).results;
+    console.log(moviesList.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  } else {
+    console.log('Error code:', response.statusCode);
+  }
+});
