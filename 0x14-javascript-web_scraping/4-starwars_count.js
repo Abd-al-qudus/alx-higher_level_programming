30a812e9fd26aa9168132e1d44c4113249958c84
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-29 04:47:07 +0100

    updated star war character count shellbang fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const API = process.argv[2];
-request.get(API, function (error, response, body) {
+request(API, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else if (response.statusCode === 200) {
-    const moviesList = JSON.parse(body).results;
-    console.log(moviesList.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
   } else {
-    console.log('Error code:', response.statusCode);
+    const moviesList = JSON.parse(body).results;
+    console.log(moviesList.reduce((charCount, movies) => {
+      return movies.characters.find((character) => character.endsWith('/18/'))
+        ? charCount + 1
+        : charCount;
+    }, 0));
   }
 });
