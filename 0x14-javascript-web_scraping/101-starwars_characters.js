1dff38c7f2dbf0b763390835d8be8ef6da1c1924
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-29 08:43:51 +0100

    checked async request for task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,12 +3,11 @@ const request = require('request');
 const charId = process.argv[2];
 const moviesAPI = 'https://swapi-api.alx-tools.com/api/films/';
 
-function getName(url, callback){
-  request(url, (error, resp, body)=>{
-    if (error){
+function getName (url, callback) {
+  request(url, (error, resp, body) => {
+    if (error) {
       console.log(error);
-    } 
-    else{
+    } else {
       const charName = JSON.parse(body).name;
       callback(null, charName);
     }
@@ -21,8 +20,8 @@ request(moviesAPI + charId, function (error, resp, body) {
   } else {
     const charactersList = JSON.parse(body).characters;
     for (let i = 0; i < charactersList.length; i++) {
-      getName(charactersList[i], (error, data)=>{
-        if (error){
+      getName(charactersList[i], (error, data) => {
+        if (error) {
           console.log(error);
         }
         console.log(data);
