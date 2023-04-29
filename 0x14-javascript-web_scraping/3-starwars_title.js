fe7f80f55ae945085003a4c7b5c53a62d270754a
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-29 04:04:12 +0100

    uploaded star war api title shellbang fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const API = 'https://swapi-api.alx-tools.com/api/films/';
 const id = process.argv[2];
 const requestUrl = API + id;
-request.get(requestUrl).on('response', function (error, response, body) {
+request.get(requestUrl, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
