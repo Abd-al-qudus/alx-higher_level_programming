0b0d678d6d26793e3497effe82a9f4b17dc0f6eb
Abd-al-qudus <jimohqudus86@gmail.com>
2023-04-29 05:43:40 +0100

    scrap and write web fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,11 +3,4 @@ const request = require('request');
 const fs = require('fs');
 const API = process.argv[2];
 const fileName = process.argv[3];
-request(API, function(error, response, body){
-  if (error) {
-    console.log(error);
-  }
-  else{
-    fs.writeFile(fileName, body, 'utf-8');
-  }
-});
+request(API).pipe(fs.createWriteStream(fileName));
